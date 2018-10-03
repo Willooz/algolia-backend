08b5263d9cc0df159643d2a6ba23951f5b6a9e6e
Willem Browne <willem.browne@gmail.com>
2018-10-03 16:55:33 +0200

    Added algolia config

--- src/services/apps/apps.service.js
+++ src/services/apps/apps.service.js
@@ -3,11 +3,10 @@ const createService = require('./apps.class.js');
 const hooks = require('./apps.hooks');
 
 module.exports = function (app) {
-  
-  const paginate = app.get('paginate');
 
   const options = {
-    paginate
+    applicationId: app.get('algolia').applicationId
+    apiKey: app.get('algolia').apiKey
   };
 
   // Initialize our service with any options it requires
